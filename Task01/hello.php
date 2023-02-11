715c4011cc2532ad530cbaaf93a5610bb99992c4
BTatyana7 <tania.bourlakova2002@yandex.ru>
2023-02-11 13:44:49 +0300

    Task01

---
+++ Task01/hello.php
@@ -0,0 +1,8 @@
+<?php
+date_default_timezone_set('Etc/GMT-3');
+echo "Текущая дата: ";
+echo date ("d.m.Y"), "\n"; 
+echo "Текущее время: ";
+echo (date("H:i:s")), "\n";
+echo "Бурлакова Татьяна";
+?>
